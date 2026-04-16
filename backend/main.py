686ff77cd639cf277552efba1d8c6b81935c8cdc
VIRAJ261-cloud <darkknightthe91@gmail.com>
2026-04-16 13:08:34 +0530

    create and link frontend

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI,HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from modelImport import load_models
 from predict import predictIt
 from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://127.0.0.1:3000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],    
+    allow_headers=["*"],   
+)
 
 class Text(BaseModel):
     text : str
